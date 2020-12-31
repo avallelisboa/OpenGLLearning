c72ae81f953586c1767025d85372241c74fbe6b8
Andres Valle Lisboa <andresvallelisboa@outlook.com>
2020-12-31 03:09:10 -0300

    image can be translated with imgui

--- Renderer/Render.cpp
+++ Renderer/Render.cpp
@@ -29,14 +29,7 @@ void Renderer::Draw(VertexArray& va, IndexBuffer& ib, Shaders& shader){
 	va.Bind();
 	ib.Bind();
 
-	glm::mat4 projectionMat = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::mat4 viewMat = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-	glm::mat4 modelMat = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-	glm::mat4 mvp = projectionMat * viewMat * modelMat;
-	shader.Bind();
-	shader.SetUniformMat4f("u_MVP", mvp);
-	shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f );
+	//shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f );
 
 	Texture texture("Textures/src/Obi-Wan.png");
 	texture.Bind();
--- main.cpp
+++ main.cpp
@@ -99,8 +99,17 @@ int main(void)
 
     Shaders shader("./Shaders/src/vertex.shader", "./Shaders/src/fragment.shader");
     shader.Bind();
-    Renderer renderer;
 
+    glm::vec3 translation(200, 200, 0);
+
+    glm::mat4 projectionMat = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    glm::mat4 viewMat = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 modelMat = glm::translate(glm::mat4(1.0f), translation);
+    glm::mat4 mvp = projectionMat * viewMat * modelMat;
+
+    shader.SetUniformMat4f("u_MVP", mvp);
+
+    Renderer renderer;
     renderer.Draw(vertexArray, indexBuffer, shader);
 
     ImGui::CreateContext();
@@ -144,44 +153,21 @@ int main(void)
         ImGui::NewFrame();
 
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             static float f = 0.0f;
-            static int counter = 0;
 
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-
 
         // Rendering
         ImGui::Render();
@@ -189,6 +175,14 @@ int main(void)
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+
+        projectionMat = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        viewMat = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        modelMat = glm::translate(glm::mat4(1.0f), translation);
+        mvp = projectionMat * viewMat * modelMat;
+
+        shader.Bind();
+        shader.SetUniformMat4f("u_MVP", mvp);
         
         renderer.Draw(vertexArray, indexBuffer, shader);
 
